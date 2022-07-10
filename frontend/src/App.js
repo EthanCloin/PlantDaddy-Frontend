22eaa8396dae8ee5fbeaed8c0ac418b318e43576
Ethan Cloin <psnethan@gmail.com>
2022-07-10 19:05:46 -0400

    add materialui button to App

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+// import { ContainedButton } from 'components/ContainedButton';
+import { Button } from '@mui/material';
+
+const App = () => {
+
+    return (
+        <div>
+            <Button variant="contained">My Button</Button>
+        </div>
+    )
+};
+
+const getAllPlants = async () => {
+
+}
+
+export default App
+
