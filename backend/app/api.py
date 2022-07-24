4461864b948d4ab278ec82aa6ad2b5435a7ed49a
Ethan Cloin <psnethan@gmail.com>
2022-07-24 16:35:53 -0400

    add get by health

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,7 +1,7 @@
 import os
 from fastapi import FastAPI, HTTPException, Query
 from fastapi.responses import HTMLResponse
-from .models import Plant, PlantCreate, PlantRead
+from .models import Plant, PlantCreate, PlantRead, Health
 from sqlmodel import create_engine, SQLModel, Session, select
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
@@ -12,16 +12,13 @@ SQLITE_FILE = os.environ.get("SQLITE_FILE", "")
 
 # api setup
 app = FastAPI()
-origins = [
-    "http://localhost:3000",
-    "localhost:3000"
-]
+origins = ["http://localhost:3000", "localhost:3000"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 
@@ -44,7 +41,7 @@ def on_startup():
     create_db_and_tables()
 
 
-@app.post("/plants/", response_model=PlantRead)
+@app.post("/plants/new/", response_model=PlantRead)
 async def create_new_plant(plant: PlantCreate):
     with Session(engine) as session:
         db_plant = Plant.from_orm(plant)
@@ -55,23 +52,15 @@ async def create_new_plant(plant: PlantCreate):
 
 
 @app.get("/nursery/")
-async def get_all_plants(offset: int = 0, limit: int = Query(default=10, lte=100)) -> list[Plant]:
+async def get_all_plants(
+    offset: int = 0, limit: int = Query(default=10, lte=100)
+) -> list[Plant]:
     with Session(engine) as session:
         plants = session.exec(select(Plant).offset(offset).limit(limit)).all()
         return plants
-    # return """
-    # <html>
-    #     <head>
-    #         <title>Your Nursery</title>
-    #     </head>
-    #     <body>
-    #         <h1>Your plants live here!</h1>
-    #     </body>
-    # </html>
-    # """
 
 
-@app.get("/plants/{plant_id}", response_model=PlantRead)
+@app.get("/plants/id/{plant_id}", response_model=PlantRead)
 async def get_plant_by_id(plant_id: int):
     with Session(engine) as session:
         plant = session.get(Plant, plant_id)
@@ -79,3 +68,19 @@ async def get_plant_by_id(plant_id: int):
             raise HTTPException(status_code=404, detail="Plant not found")
         return plant
 
+
+@app.get("/plants/health/{plant_health}", response_model=list[Plant])
+async def get_plants_by_health(
+    plant_health: Health, offset: int = 0, limit: int = Query(default=10, lte=100)
+):
+
+    with Session(engine) as session:
+        stmt = (
+            select(Plant)
+            .where(Plant.health == plant_health)
+            .offset(offset)
+            .limit(limit)
+        )
+        result = session.exec(stmt)
+        plants = result.all()
+        return plants
