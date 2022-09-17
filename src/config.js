995fc01e5cf02505e29ba8511ba3301e19126f11
Ethan Cloin <psnethan@gmail.com>
2022-09-17 14:52:31 -0400

    resolve conflicts?

---
+++ src/config.js
@@ -0,0 +1,9 @@
+// import runtimeEnv from '@mars/heroku-js-runtime-env';
+
+// const env = runtimeEnv();
+const config = {
+  apiBasePath: 'http://0.0.0.0:8000',
+  reactAppMode: 'dev',
+};
+
+export default config;
