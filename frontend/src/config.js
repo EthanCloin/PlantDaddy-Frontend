4dc5dbe7949ce0bcd284022f8026b5dbfe9a7c39
Ethan Cloin <psnethan@gmail.com>
2022-09-17 14:47:57 -0400

    remove backend and ensure this still runs

--- frontend/src/config.js
+++
@@ -1,9 +0,0 @@
-// import runtimeEnv from '@mars/heroku-js-runtime-env';
-
-// const env = runtimeEnv();
-const config = {
-  apiBasePath: 'http://0.0.0.0:8000',
-  reactAppMode: 'dev',
-};
-
-export default config;
