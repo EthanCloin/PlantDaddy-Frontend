f83391ab0792b52ac903bd2868c49b3b0ec3576a
Ethan Cloin <psnethan@gmail.com>
2022-10-15 11:29:27 -0400

    add some content to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,15 @@
 import "./Header.css";
 
 const Header = () => {
-  return <div className="Header">Header works</div>;
+  return (
+    <div className="Header">
+      <h1 className="app-title">Plant Daddy</h1>
+      <div className="page-links">
+        <p>Home</p>
+        <p>Thirsty</p>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
